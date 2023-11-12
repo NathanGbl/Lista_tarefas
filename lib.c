4a765fc9e7a2fb50a27915accfbf3199efa97609
NathanGbl <nathangabriel.f.leite@gmail.com>
2023-11-12 13:28:40 -0300

    Versão final do projeto Lista_Tarefa.

--- lib.c
+++ lib.c
@@ -200,6 +200,277 @@ void alterar_tarefa(lista_tarefa *lt, int *opcao) {
   }
   
 }
+  
+
+// filtros
+
+void filtro_prioridade(lista_tarefa *lt) {
+
+  int prioridade;
+  int cont = 0;
+  printf("\t\tDigite a prioridade desejada: ");
+  scanf("%d", &prioridade);
+  
+  for (int i = 0; i < lt->qtnd; i++) {
+    // Loop que itera sobre todas as tarefas do struct lista_tarefa. Caso a tarefa tenha a prioridade digitada pelo usuário, a exibe.
+    if (lt->tarefa[i].prioridade == prioridade) {
+      printf("\t\tTarefa %d\n", i);
+      printf("\t\t\tDescrição: %s\n", lt->tarefa[i].descricao);
+      printf("\t\t\tCategoria: %s\n", lt->tarefa[i].categoria);
+      printf("\t\t\tPrioridade: %d\n", lt->tarefa[i].prioridade);
+      printf("\t\t\tEstado: %s\n", lt->tarefa[i].estado);
+      cont += 1;
+    }
+  }
+  if (cont == 0) {
+    printf("\t\tNão há tarefas com a prioridade %d.\n", prioridade);
+  }
+  
+}
+
+void filtro_estado(lista_tarefa *lt, int *opcao) {
+
+  int cont = 0;
+  menu_estado(opcao, 1);
+  // Em cada if muda apenas a comparação entre os estados
+  // De acordo com a opcao, o loop dentro dos if/else if verifica se a tarefa tem o estado escolhido e exibe em caso afirmativo
+  if (*opcao == 1) {
+    for (int i = 0; i < lt->qtnd; i++) {
+      if (strcmp(lt->tarefa[i].estado, "Completo") == 0) {
+        printf("\t\tTarefa %d\n", i);
+        printf("\t\t\tDescrição: %s\n", lt->tarefa[i].descricao);
+        printf("\t\t\tCategoria: %s\n", lt->tarefa[i].categoria);
+        printf("\t\t\tPrioridade: %d\n", lt->tarefa[i].prioridade);
+        printf("\t\t\tEstado: %s\n", lt->tarefa[i].estado);
+        cont += 1;
+      }
+    }
+    if (cont == 0) {
+      printf("\t\tNão existe tarefa registrada com esse estado.\n");
+    }
+  }
+  else if (*opcao == 2) {
+    for (int i = 0; i < lt->qtnd; i++) {
+      if (strcmp(lt->tarefa[i].estado, "Em andamento") == 0) {
+        printf("\t\tTarefa %d\n", i);
+        printf("\t\t\tDescrição: %s\n", lt->tarefa[i].descricao);
+        printf("\t\t\tCategoria: %s\n", lt->tarefa[i].categoria);
+        printf("\t\t\tPrioridade: %d\n", lt->tarefa[i].prioridade);
+        printf("\t\t\tEstado: %s\n", lt->tarefa[i].estado);
+        cont += 1;
+      }
+    }
+    if (cont == 0) {
+      printf("\t\tNão existe tarefa registrada com esse estado.\n");
+    }
+  }
+  else if (*opcao == 3) {
+    for (int i = 0; i < lt->qtnd; i++) {
+      if (strcmp(lt->tarefa[i].estado, "Não iniciado") == 0) {
+        printf("\t\tTarefa %d\n", i);
+        printf("\t\t\tDescrição: %s\n", lt->tarefa[i].descricao);
+        printf("\t\t\tCategoria: %s\n", lt->tarefa[i].categoria);
+        printf("\t\t\tPrioridade: %d\n", lt->tarefa[i].prioridade);
+        printf("\t\t\tEstado: %s\n", lt->tarefa[i].estado);
+        cont += 1;
+      }
+    }
+    if (cont == 0) {
+      printf("\t\tNão existe tarefa registrada com esse estado.\n");
+    }
+  }
+  else {
+    printf("\t\t\tOpção inválida\n"); // Tratamento de erro
+  }
+  
+}
+
+void filtro_categoria(lista_tarefa *lt) {
+  
+  char categoria[101];
+  int cont = 0;
+  printf("\t\tDigite a categoria desejada: ");
+  clean_buffer(); // Para que o scanf não registre o \n como input
+  scanf("%[^\n]", categoria);
+
+  for (int i = 0; i < lt->qtnd; i++) {
+    // Loop que itera sobre todas as tarefas do struct lista_tarefa
+    // Se a categoria por a digita pelo usuário, imprime-a
+    if (strcmp(lt->tarefa[i].categoria, categoria) == 0) {
+      cont += 1;
+      printf("\t\tTarefa %d\n", i);
+      printf("\t\t\tDescrição: %s\n", lt->tarefa[i].descricao);
+      printf("\t\t\tCategoria: %s\n", lt->tarefa[i].categoria);
+      printf("\t\t\tPrioridade: %d\n", lt->tarefa[i].prioridade);
+      printf("\t\t\tEstado: %s\n", lt->tarefa[i].estado);
+    }
+  }
+  if (cont == 0) {
+    printf("\t\tCategoria não existe\n");
+  }
+  
+}
+
+int compara_prioridade(const void *valor1, const void *valor2) {
+
+  // função usava para o qsort da função filtro_categoria_prioridade
+  // Essa função reorganiza de forma decrescente as prioridades de cada tarefa
+  int prioridade1 = (*(tarefa *)valor1).prioridade; // Acessa a variável prioridade do struct tarefa na posição do *valor1
+  int prioridade2 = (*(tarefa *)valor2).prioridade; // Acessa a variável prioridade do struct tarefa na posição do *valor2
+
+  if (prioridade1 > prioridade2) {
+    return -1;
+  }
+  else if (prioridade1 < prioridade2) {
+    return 1;
+  }
+  else {
+    return 0;
+  }
+  
+}
+
+void filtro_categoria_prioridade(lista_tarefa *lt, int modo) {
+
+  tarefa lt_categoria[100];  
+  int lt_categoria_qtnd = 0;  
+  char categoria[101];
+
+  printf("\t\tCategoria desejada: ");
+  clean_buffer(); // Para que o scanf não registre o \n como input
+  scanf("%[^\n]", categoria);
+
+  for (int i = 0; i < lt->qtnd; i++) {
+    // Loop que itera sobre todas as tarefas do struct lista_tarefa
+    // Caso a categoria for igual a digitada pelo usuário, adiciona-a na lista lt_categoria
+    if (strcmp(lt->tarefa[i].categoria, categoria) == 0) {
+    strcpy(lt_categoria[lt_categoria_qtnd].descricao, lt->tarefa[i].descricao);
+    strcpy(lt_categoria[lt_categoria_qtnd].categoria, lt->tarefa[i].categoria);
+    lt_categoria[lt_categoria_qtnd].prioridade = lt->tarefa[i].prioridade;
+    strcpy(lt_categoria[lt_categoria_qtnd].estado, lt->tarefa[i].estado);
+    lt_categoria_qtnd += 1; // Aumenta a quantidade para apontar para a última tarefa
+    }
+  }
+
+  qsort(lt_categoria, lt_categoria_qtnd, sizeof(tarefa), compara_prioridade); // Ordena de forma decrescente o array lt_categoria com base na prioridade
+
+  if (modo == 0) {
+    // Se for apenas para imprimir, o loop itera sobre a lista lt_categoria e imprime as tarefas dentro dela
+    for (int i = 0; i < lt_categoria_qtnd; i++) {
+      printf("\t\tTarefa %d\n", i);
+      printf("\t\t\tDescrição: %s\n", lt_categoria[i].descricao);
+      printf("\t\t\tCategoria: %s\n", lt_categoria[i].categoria);
+      printf("\t\t\tPrioridade: %d\n", lt_categoria[i].prioridade);
+      printf("\t\t\tEstado: %s\n", lt_categoria[i].estado);
+    }
+    
+    if (lt_categoria_qtnd == 0) {
+      printf("\t\tCategoria não existe\n");
+    }
+  }
+  else if (modo == 1) {
+    // Se for para escrever no arquivo, abrirá o arquivo Lista_tarefa.txt para escrever
+    // O loop itera sobre a lista lt_categoria e escreve as tarefas dentro dela no arquivo
+    FILE *f = fopen("Lista_tarefas.txt", "w");
+    fprintf(f, "Tarefa           Prioridade               Categoria               estado         descricao\n"); // Digita a primeira linha do arquivo
+
+    for (int i = 0; i < lt_categoria_qtnd; i++) {
+      fprintf(f, "Tarefa %d:            %d                    %s                 %s           %s\n", i, lt_categoria[i].prioridade, lt_categoria[i].categoria, lt_categoria[i].estado, lt_categoria[i].descricao);
+    }
+    fclose(f); // Fecha o arquivo
+
+    if (lt_categoria_qtnd == 0) {
+      printf("\t\tCategoria não existe\n");
+    }
+  }
+
+}
+
+// exportar
+
+void exportar_prioridade(lista_tarefa *lt) {
+
+  int prioridade;
+  int cont = 0;
+  printf("\t\tPrioridade desejada: ");
+  scanf("%d", &prioridade);
+  
+  if (prioridade < 0 || prioridade > 10) {
+    // Valida a prioridade digitada pelo usuário
+    printf("\tPrioridade inválida\n");
+  }
+  else {
+    FILE *f = fopen("Lista_tarefas.txt", "w"); // Abre o arquivo para escrever
+    fprintf(f, "Tarefa          Prioridade          Categoria          estado          descricao\n"); // Digita a primeira linha do arquivo
+    for (int i = 0; i < lt->qtnd; i++) {
+      // Loop itera sobre todas as tarefas do struct lista_tarefa e coloca no arquivo as tarefas com prioridade digitada pelo usuário
+      if (lt->tarefa[i].prioridade == prioridade) {
+        fprintf(f, "Tarefa %d:            %d                    %s                 %s           %s\n", i, lt->tarefa[i].prioridade, lt->tarefa[i].categoria, lt->tarefa[i].estado, lt->tarefa[i].descricao);
+        cont += 1;
+      }
+    }
+    fclose(f); // Fecha o arquivo
+    if (cont == 0) {
+      printf("\t\tPrioridade não registrada ainda.\n");
+    }
+  }
+  
+}
+
+void exportar_categoria(lista_tarefa *lt) {
+
+  char categoria[101];
+  int cont = 0;
+  printf("\t\tCategoria desejada: ");
+  clean_buffer(); // Para que o scanf não registre o \n como input
+  scanf("%[^\n]", categoria);
+  
+  FILE *f = fopen("Lista_tarefas.txt", "w");
+  fprintf(f, "Tarefa          Prioridade          Categoria          estado          descricao\n"); // Digita a primeira linha do arquivo
+  for (int i = 0; i < lt->qtnd; i++) {
+    if (strcmp(lt->tarefa[i].categoria, categoria) == 0) {
+      // Loop itera sobre todas as tarefas do struct lista_tarefa e coloca no arquivo as tarefas com categoria digitada pelo usuário
+      fprintf(f, "Tarefa %d:            %d                    %s                 %s           %s\n", i, lt->tarefa[i].prioridade, lt->tarefa[i].categoria, lt->tarefa[i].estado, lt->tarefa[i].descricao);
+      cont += 1;
+    }
+  }
+  fclose(f); // Fecha o arquivo
+  if (cont == 0) {
+    printf("\t\tCategoria não registrada ainda.\n");
+  }
+  
+}
+
+void exportar_categoria_prioridade(lista_tarefa *lt) {
+
+  filtro_categoria_prioridade(lt, 1); // Chama a função no modo de escrever txt
+  
+}
+
+void menu_filtros(int *opcao) {
+
+  // Exibe um menu de opções que o usuário pode filtrar as tarefas
+  printf("\tEscolha um dos filtros abaixo:\n");
+  printf("\t1. Filtrar por prioridade\n");
+  printf("\t2. Filtrar por estado\n");
+  printf("\t3. Filtrar por categoria\n");
+  printf("\t4. Filtrar por categoria e prioridade\n");
+  printf("\tOpção: ");
+  scanf("%d", opcao);
+
+}
+
+void menu_exportar(int *opcao) {
+
+  // Exibe um menu de opções que o usuário pode exportar as tarefas
+  printf("\tEscolha a forma de exportar as tarefas:\n");
+  printf("\t1. Exportar por prioridade\n");
+  printf("\t2. Exportar por categoria\n");
+  printf("\t3. Exportar por categoria e prioridade\n");
+  printf("\tOpção: ");
+  scanf("%d", opcao);
+
+}
 
 void le_arquivo(lista_tarefa *lt) {
 
